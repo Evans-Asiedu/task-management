06ed71193020bc921420cd391742f35e5ed65be8
zeynab.mvs <zeynab.mvs@gmail.com>
2025-03-06 19:52:59 +0330

    Configure vite t use absolute paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import {data} from './data.js'
-function App() {
+import { data } from "src/data";
 
+function App() {
   return (
     <>
-     <h1 className='text-3xl font-bold underline text-red-500 font-jakarta'>Test tailwind</h1>
-     {data.loggedInUser.name}
-     <img src={data.tasks[0].imageUrl} alt=""/>
+      <h1 className="text-3xl font-bold underline text-red-500 font-jakarta">
+        Test tailwind
+      </h1>
+      {data.loggedInUser.name}
+      <img src={data.tasks[0].imageUrl} alt="" />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
