855010a11fa345656e96f446714c24dac988814f
Hameed Khan <khanhameed989@gmail.com>
2025-03-10 01:43:22 +0530

    Fixes css issue of RunningTask comp

--- src/components/RunningTask.jsx
+++ src/components/RunningTask.jsx
@@ -11,12 +11,12 @@ const RunningTask = () => {
   return (
     <div className='m-4 w-[327px] h-[124px] sm:w-[200px] sm:h-[220px] flex sm:flex-col justify-between shadow-xl p-4 bg-secondary-900 text-white rounded-[10px] transition duration-500' >
 
-      <div className='flex flex-col gap-[18px]'>
-        <h2 className='text-[16px] font-semibold text-primary-0 '>Running Task</h2>
-        <h2 className='text-[32px] font-semibold text-primary-0 '>65</h2>
+      <div className='flex flex-col gap-5'>
+        <h2 className='text-base font-semibold text-primary-0 '>Running Task</h2>
+        <h2 className='text-3xl font-semibold text-primary-0 '>65</h2>
       </div>
 
-      <div className='flex items-center gap-[18px]'>
+      <div className='flex items-center gap-5'>
         <div className="flex items-center justify-center">
           <svg width={size} height={size} className="transform -rotate-90">
             {/* Background Circle */}
@@ -41,12 +41,12 @@ const RunningTask = () => {
           </svg>
           {/* Progress Text */}
           <div className="absolute text-center">
-            <span className="text-lg font-bold text-primary-0">{progress}%</span>
+            <span className="text-lg font-medium  text-primary-0">{progress}%</span>
           </div>
         </div>
         <div className='flex flex-col justify-center items-center'>
-          <h2 className='text-[20px] font-bold'>100</h2>
-          <p className='text-[14px] font-bold text-secondary-300 '>Task</p>
+          <h2 className='text-xl font-semibold'>100</h2>
+          <p className='text-base font-medium text-secondary-300 '>Task</p>
         </div>
       </div>
     </div>
