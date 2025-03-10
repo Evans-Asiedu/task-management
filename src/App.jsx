0a3700bb8658849c8d8ccd673e8481d9da723cf1
Evans-Asiedu <evansasiedu352@gmail.com>
2025-03-10 21:20:06 +0000

    Rename task_card to TaskCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { data } from "src/data";
 import RunningTask from "./components/RunningTask";
 import { Calendar } from "./components/Calendar";
-import { TaskCard } from "./components/task_card";
+import { TaskCard } from "./components/TaskCard";
 import TaskDetails from "./components/TaskDetails";
 
 function App() {
