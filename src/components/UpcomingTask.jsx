436a258dbbb2d4de74201b06f6a167858f024df8
Evans-Asiedu <evansasiedu352@gmail.com>
2025-03-18 20:39:56 +0000

    Refactor header of UpcomingTask component to build CardHeader component

--- src/components/UpcomingTask.jsx
+++ src/components/UpcomingTask.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import TaskCard from "./TaskCard";
+import CardHeader from "./CardHeader";
 import { data } from "src/data";
 
 const UpcomingTask = () => {
@@ -23,29 +24,13 @@ const UpcomingTask = () => {
   return (
     <div className="max-w-[327px] lg:max-w-[688px] mx-4">
       {/* Header */}
-      <div className="flex justify-between items-center mb-3">
-        <h2 className="text-lg font-semibold mx-1">Upcoming Task</h2>
-        <button className="text-gray-500 text-lg mx-1">
-          <i
-            className={`fa fa-angle-left fa-2x mx-3 ${
-              startIndex === 0
-                ? "opacity-50 cursor-not-allowed"
-                : "cursor-pointer"
-            }`}
-            aria-hidden="true"
-            onClick={handlePrevious}
-          ></i>
-          <i
-            className={`fa fa-angle-right fa-2x ${
-              startIndex + taskPerView >= tasks.length
-                ? "opacity-50 cursor-not-allowed"
-                : "cursor-pointer"
-            }`}
-            aria-hidden="true"
-            onClick={handleNext}
-          ></i>
-        </button>
-      </div>
+      <CardHeader
+        start={startIndex}
+        current={startIndex + taskPerView}
+        total={tasks.length}
+        onNext={handleNext}
+        onPrevious={handlePrevious}
+      />
       <div
         className="flex flex-row transition-transform duration-300 ease-in-out"
         // style={{
