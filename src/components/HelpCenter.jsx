bbfaed901fcfe5bd78b765f1a5242e4bcbec7386
Evans-Asiedu <evansasiedu352@gmail.com>
2025-03-27 16:58:19 +0000

    Add the two circles to the background of HelpCenter component

--- src/components/HelpCenter.jsx
+++ src/components/HelpCenter.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
-const HelpCenter = ({className}) => {
+const HelpCenter = ({ className }) => {
   return (
     <div className={`${className} max-w-[188px] h-[274px] relative`}>
-      <div className="h-[52px] w-[52px] bg-white rounded-full border border-primary-100 absolute left-1/2 -translate-x-1/2 z-1">
+      <div className="h-[160px] w-[160px] rounded-full bg-primary-0/10 absolute -translate-y-1/2 -translate-x-3/5 z-10"></div>
+      <div className="h-[160px] w-[160px] rounded-full bg-primary-0/10 absolute bottom-0 right-0 translate-y-11/20 translate-x-3/5 z-10"></div>
+      <div className="h-[52px] w-[52px] rounded-full bg-primary-0 border border-primary-100 absolute left-1/2 -translate-x-1/2 z-10">
         <i
           class="absolute top-0.5 left-1/2 -translate-x-1/2 fa fa-question-circle fa-3x"
           aria-hidden="true"
@@ -17,7 +19,7 @@ const HelpCenter = ({className}) => {
             questions
           </p>
         </div>
-        <button className="text-[12px] bg-white text-seondary-500 rounded-2xl py-2 px-2.5 cursor-pointer">
+        <button className="text-[12px] bg-primary-0 text-seondary-500 rounded-2xl py-2 px-2.5 cursor-pointer">
           {" "}
           Go to Help Center
         </button>
