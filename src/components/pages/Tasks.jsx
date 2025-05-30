e867628f5c8c4abd60f4c006fe80243c50094089
william2544 <onyangowilliam2002@gmail.com>
2025-05-30 18:50:33 +0300

    done with tasks page

--- src/components/pages/Tasks.jsx
+++ src/components/pages/Tasks.jsx
@@ -1,11 +1,23 @@
 import Header from "src/components/layout/Header";
+import UpcomingTask from "../UpcomingTask";
+import SliderHeader from "../SliderHeader";
 
 const Tasks = () => {
   return (
     <>
-      <Header showActions={true} title="Tasks" />
+      <Header showActions={true} title="Explore Tasks" />
       <main className="h-full flex bg-n-2">
-        <h1>Tasks page</h1>
+        <div className="px-6 lg:px-8 space-y-6 lg:space-y-4">
+          <section className="space-y-6">
+            {/* Shrink each UpcomingTask */}
+            <div>
+              <UpcomingTask taskPerViewOverride={3} sliderHeaderProps={{title:'Time Limit'}} />
+            </div>
+            <div>
+              <UpcomingTask taskPerViewOverride={3} sliderHeaderProps={{title:'New Tasks'}}/>
+            </div>
+          </section>
+        </div>
       </main>
     </>
   );
