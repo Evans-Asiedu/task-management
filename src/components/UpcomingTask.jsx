efec4715886af43513052735c6c2159bd57edb76
Zeynab Movasaqpour <zeynab.mvs@gmail.com>
2025-10-26 14:24:55 +0330

    Refactor MonthlyMentors and UpcomingTask components to use a unified Slider component, to simplify the code and enhances maintainability.

--- src/components/UpcomingTask.jsx
+++ src/components/UpcomingTask.jsx
@@ -1,67 +1,22 @@
-import React, { useState, useEffect } from "react";
 import { data } from "src/data";
-import SliderHeader from "components/SliderHeader";
-import SliderBody from "components/SliderBody";
+import Slider from "src/components/slider/Slider";
 import TaskCard from "components/TaskCard";
 
 const UpcomingTask = () => {
   const tasks = data.tasks;
-  const [startIndex, setStartIndex] = useState(0);
-  const [taskPerView, setTaskPerView] = useState(1);
-
-  // function to check screen size
-  useEffect(() => {
-    const updateTaskPerView = () => {
-      if (window.matchMedia("(min-width: 1024px)").matches) {
-        setTaskPerView(2);
-      } else {
-        setTaskPerView(1);
-      }
-    };
-
-    updateTaskPerView();
-    window.addEventListener("resize", updateTaskPerView);
-
-    return () => window.removeEventListener("resize", updateTaskPerView);
-  }, []);
-
-  const start = startIndex + 1;
-  const current = startIndex + taskPerView;
-  const end = tasks.length;
-
-  const handlePrevious = () => {
-    if (startIndex > 0) {
-      setStartIndex(startIndex - 1);
-    }
-  };
-
-  const handleNext = () => {
-    if (startIndex + taskPerView < tasks.length) {
-      setStartIndex(startIndex + 1);
-    }
-  };
 
   const taskCard = (task) => {
     return <TaskCard task={task} />;
   };
 
   return (
-    <div>
-      <SliderHeader
-        title={"Upcoming Task"}
-        onNext={handleNext}
-        onPrevious={handlePrevious}
-        start={start}
-        current={current}
-        end={end}
-      />
-      <SliderBody
-        slides={tasks}
-        startIndex={startIndex}
-        slidePerView={taskPerView}
-        component={taskCard}
-      />
-    </div>
+    <Slider
+      title="Upcoming Task"
+      data={tasks}
+      component={taskCard}
+      itemsPerViewDesktop={2}
+      itemsPerViewMobile={1}
+    />
   );
 };
 
